0d278ccafeb252d01e1980b8f2b90d2355ca7001
Phillip Friedrich <friedrich.phillip@gmail.com>
2014-01-29 15:22:25 +0100

    first

---
+++ src/main/java/com/example/tutorial/config/ApplicationContextConfig.java
@@ -0,0 +1,29 @@
+package com.example.tutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.tutorial")
+/*@PropertySource("classpath:com/example/tutorial/config/application.properties")*/
+public class ApplicationContextConfig extends WebMvcConfigurerAdapter{
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Bean
+	public InternalResourceViewResolver configureInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
